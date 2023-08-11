3efdbbe84ec1e4c3c0a28f403e8c1b13b323f96d
ruma2100 <ruma2100@student.miun.se>
2023-08-11 12:41:20 +0200

    Refactored measuring functions to use a return value instead

--- main.py
+++ main.py
@@ -14,60 +14,70 @@ import modules.soundsensor as Soundsensor
 # global variables
 period = 0              # update periode in seconds for measuring a sending
 
-hum = None
-temp = None
-light = None
-windspeed = None
 
 led = Pin(machine.Pin.exp_board.G15,mode=Pin.OUT)
 led.value(0)
 UP_TIME = 15
 DOWN_TIME = 60
 
-def measure_sound():
+def measure_sound(apin_sensor) -> int:
     adc = machine.ADC()             # create an ADC object for the sound sensor
-    avg_sound = Soundsensor.running_average(apin_soundsensor)
-    print("Sound: ", avg_sound)
-    #time.sleep(periode)
+    avg_sound = Soundsensor.running_average(apin_sensor)
+    if avg_sound == None:
+        avg_sound = 0
+    return avg_sound
 
-def measure_dht():
+def measure_dht(sensor) -> list:
     #print("function measure")
-    global d
-    global hum, temp
+    #global d
+    #global hum, temp
+    hum = 0
+    temp = 0
     # measure DHT temp and hum values
-    if d.trigger() == True:
-        hum = d.humidity
-        temp = d.temperature
-        print("Humidity:", hum,  "Temperature: ",temp)
-        
-    else:
-        print("STATUS: ",d.status)
-        print(None, None)
+    for _ in range(10):
+        if d.trigger() == True:
+            hum = d.humidity
+            temp = d.temperature
+            print("Humidity:", hum)
+            print("Temperature: ",temp)
+            return [hum, temp]
+            
+        else:
+            print("STATUS: ",d.status)
+            print(None, None)
+            time.sleep(1)
+           
+    return [0,0]
     time.sleep(1)
 
-    #return(hum, temp)
+    
 
-def measure_light():
-    global apin_lightsensor
-    global light
+def measure_light(apin_sensor) -> int:
+    #global apin_lightsensor
+    #global light
 
-    light = apin_lightsensor()
+    light = apin_sensor
     if light != None:
         print("Light: ", light)
     else:
         print("Light: ",None)
+        light = 0
+    return light
     #time.sleep(5)
 
-# def measure_wind():
-    global Anemometer
-    global windspeed
-
-    if windspeed != None:
-        windspeed = sensor_anemometer.get_windspeed()
-        
+def measure_wind(sensor):
+    #global Anemometer
+    #global windspeed
+    windspeed = 0
+    if sensor != None:
+        windspeed = sensor.get_windspeed()
         print("Windspeed", windspeed)
+        
     else:
+        windspeed = 0
         print("Windspeed:", None)
+
+    return windspeed
 
 # def measure():
 #     hum = 0
@@ -107,53 +117,63 @@ def measure_light():
 
 # light sensor init
 adc = machine.ADC()             # create an ADC object for the light sensor
-apin_lightsensor = adc.channel(pin='P13', attn = machine.ADC.ATTN_11DB)   # create an analog pin on P13, 3.3V reference, 12bit
-apin_soundsensor = adc.channel(pin='P13', attn = machine.ADC.ATTN_11DB)   # create an analog pin on P13
+#apin_lightsensor = adc.channel(pin='P13', attn = machine.ADC.ATTN_11DB)   # create an analog pin on P13, 3.3V reference, 12bit
 
 # anemometer init
-sensor_anemometer = Anemometer()
 
 print("starting main...")
 
 def appendPayload(Bytes) -> list:
     payload = []
-    for i in range(Bytes):
+    for i in range(len(Bytes)):
         payload.append(Bytes[i])
     return payload
 
 if __name__ == "__main__":
     sckt = join_lora()
     time.sleep(2)
-    MESURE = 0
+    
     # global data buffer
     payload = []            # common data buffer to collect and send
-    d = dht_module.device(machine.Pin.exp_board.G22)
     chrono = Timer.Chrono()
     chrono.start()
     led.value(1)
-    while chrono.read() < UP_TIME:
+
+    # Sensors
+    apin_soundsensor = adc.channel(pin='P13', attn = machine.ADC.ATTN_11DB)   # create an analog pin on P13
+    d = dht_module.device(machine.Pin.exp_board.G22)
+    #apin_lightsensor = adc.channel(pin='P13', attn = machine.ADC.ATTN_11DB)   # create an analog pin on P13, 3.3V reference, 12bit
+    apin_lightsensor = None
+    sensor_anemometer = Anemometer()
+    sensing = True
+    #while chrono.read() < UP_TIME:
+    while sensing:
         print("TIME: ", chrono.read())
-        measure_dht()
-        # measure_sound()
+        #measure_dht()
+        #measure_sound()
         time.sleep(2)
-        if hum != None and temp != None:
-            # encode
-            temp = int(temp * 10 ) + 400           # max -40°, use it as offset
-            hum = int(hum * 10)                 # 2 Bytes
-            lux = int(0 * 10)                 # 2 Bytes
-            press = int(0 / 100)              # original value is in pA 
-            ht_bytes = ustruct.pack('HHHH', temp, hum, lux, press)
 
-            payload = appendPayload(ht_bytes)
-               
+        # Data
+        hum_temp = measure_dht(d)
+        hum = hum_temp[0]
+        temp = hum_temp[1]
+        light = measure_light(apin_lightsensor)
+        windspeed = measure_wind(None)
+        sound = measure_sound(apin_soundsensor)
 
+       
+        # encode
+        temp = int(temp * 10 ) + 400           # max -40°, use it as offset
+        hum = int(hum * 10)                 # 2 Bytes
+        lux = int(0 * 10)                 # 2 Bytes
+        press = int(0 / 100)              # original value is in pA 
+        ht_bytes = ustruct.pack('HHHH', temp, hum, lux, sound)
 
-            print("[SENT] TEMP: {} | HUM: {} | DOLK \n".format(temp, hum))
+        payload = appendPayload(ht_bytes)
+            
+        print("[SENT] TEMP: {} | HUM: {} | DOLK \n".format(temp, hum))
 
-            hum = None
-            temp = None
-            light = None
-            windspeed = None
+            
 
        
         # payload = [0x01, 0x02, 0x03]
@@ -165,8 +185,13 @@ if __name__ == "__main__":
             
         else:
             print("EMPTY PAYLOAD")
+        
+        #sensing = False
+        time.sleep(DOWN_TIME)
+
 
     print("[Going into a coma]")
     led.value(0)
     machine.deepsleep(DOWN_TIME*1000)
+    
   
